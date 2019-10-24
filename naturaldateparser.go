0183e22a574c37e7aa56a0f6a20bd87ed492ea53
Jan-Christoph Meier <jan@codejunky.org>
2019-10-24 21:52:59 +0200

    Add go source code for naturaldate parser

---
+++ naturaldateparser.go
@@ -0,0 +1,117 @@
+package utils
+
+import (
+	"errors"
+	log "github.com/sirupsen/logrus"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type NaturalDateParser struct {
+	minuteRegex *regexp.Regexp
+	hourRegex   *regexp.Regexp
+	dayRegex    *regexp.Regexp
+	weekRegex   *regexp.Regexp
+	monthRegex  *regexp.Regexp
+}
+
+func CreateNaturalDateParser() *NaturalDateParser {
+	minuteRegex, err := regexp.Compile("(\\d\\d*) minutes? ago")
+	if err != nil {
+		log.WithError(err).Panic("Minute regex does not compile")
+	}
+
+	hourRegex, err := regexp.Compile("(\\d\\d*) hours? ago")
+	if err != nil {
+		log.WithError(err).Panic("Hour regex does not compile")
+	}
+
+	dayRegex, err := regexp.Compile("(\\d\\d*) days? ago")
+	if err != nil {
+		log.WithError(err).Panic("Day regex does not compile")
+	}
+
+	weekRegex, err := regexp.Compile("(\\d\\d*) weeks? ago")
+	if err != nil {
+		log.WithError(err).Panic("Week regex does not compile")
+	}
+
+	monthRegex, err := regexp.Compile("(\\d\\d*) months? ago")
+	if err != nil {
+		log.WithError(err).Panic("Month regex does not compile")
+	}
+
+	return &NaturalDateParser{
+		minuteRegex: minuteRegex,
+		hourRegex:   hourRegex,
+		dayRegex:    dayRegex,
+		weekRegex:   weekRegex,
+		monthRegex:  monthRegex,
+	}
+}
+
+func (p *NaturalDateParser) Parse(naturaldate string) (time.Time, error) {
+	minuteMatch := p.minuteRegex.FindStringSubmatch(naturaldate)
+	if minuteMatch != nil {
+		minutes, err := strconv.Atoi(minuteMatch[1])
+
+		if err != nil {
+			return time.Now(), err
+		}
+
+		return time.Now().Add(time.Minute * time.Duration(minutes) * -1), nil
+
+	}
+
+	hourMatch := p.hourRegex.FindStringSubmatch(naturaldate)
+	if hourMatch != nil {
+		hours, err := strconv.Atoi(hourMatch[1])
+
+		if err != nil {
+			return time.Now(), err
+		}
+
+		return time.Now().Add(time.Hour * time.Duration(hours) * -1), nil
+	}
+
+	if strings.Contains(naturaldate, "today") {
+		return time.Now(), nil
+	}
+
+	dayMatch := p.dayRegex.FindStringSubmatch(naturaldate)
+	if dayMatch != nil {
+		days, err := strconv.Atoi(dayMatch[1])
+
+		if err != nil {
+			return time.Now(), err
+		}
+
+		return time.Now().Add(time.Hour * 24 * time.Duration(days) * -1), nil
+	}
+
+	weekmatch := p.weekRegex.FindStringSubmatch(naturaldate)
+	if weekmatch != nil {
+		weeks, err := strconv.Atoi(weekmatch[1])
+
+		if err != nil {
+			return time.Now(), err
+		}
+
+		return time.Now().Add(time.Hour * 24 * 7 * time.Duration(weeks) * -1), nil
+	}
+
+	monthMatch := p.monthRegex.FindStringSubmatch(naturaldate)
+	if monthMatch != nil {
+		months, err := strconv.Atoi(monthMatch[1])
+
+		if err != nil {
+			return time.Now(), err
+		}
+
+		return time.Now().Add(time.Hour * 24 * 30 * time.Duration(months) * -1), nil
+	}
+
+	return time.Now(), errors.New("Could not be parsed")
+}
---
+++ naturaldateparser_test.go
@@ -0,0 +1,149 @@
+package utils
+
+import (
+	"testing"
+	"time"
+)
+
+func equalIgnoreSeconds(a time.Time, b time.Time) bool {
+	return a.Minute() == b.Minute() &&
+		a.Hour() == b.Hour() &&
+		a.Day() == b.Day() &&
+		a.Month() == b.Month() &&
+		a.Year() == b.Year()
+}
+
+func TestParseHour(t *testing.T) {
+	p := CreateNaturalDateParser()
+	res, err := p.Parse("1 hour ago")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !equalIgnoreSeconds(res, time.Now().Add(time.Hour*-1)) {
+		t.Error("Dates are not equal")
+	}
+}
+func TestParseHours(t *testing.T) {
+	p := CreateNaturalDateParser()
+	res, err := p.Parse("10 hours ago")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !equalIgnoreSeconds(res, time.Now().Add(time.Hour*-10)) {
+		t.Error("Dates are not equal")
+	}
+}
+
+func TestParseMinute(t *testing.T) {
+	p := CreateNaturalDateParser()
+	res, err := p.Parse("1 minute ago")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !equalIgnoreSeconds(res, time.Now().Add(time.Minute*-1)) {
+		t.Error("Dates are not equal")
+	}
+
+}
+
+func TestParseMinutes(t *testing.T) {
+	p := CreateNaturalDateParser()
+	res, err := p.Parse("10 minutes ago")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !equalIgnoreSeconds(res, time.Now().Add(time.Minute*-10)) {
+		t.Error("Dates are not equal")
+	}
+
+}
+
+func TestParseDay(t *testing.T) {
+	p := CreateNaturalDateParser()
+	res, err := p.Parse("1 day ago")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !equalIgnoreSeconds(res, time.Now().Add(time.Hour*24*-1)) {
+		t.Error("Dates are not equal")
+	}
+}
+
+func TestParseDays(t *testing.T) {
+	p := CreateNaturalDateParser()
+	res, err := p.Parse("2 days ago")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !equalIgnoreSeconds(res, time.Now().Add(time.Hour*24*-2)) {
+		t.Error("Dates are not equal")
+	}
+}
+
+func TestParseWeek(t *testing.T) {
+	p := CreateNaturalDateParser()
+	res, err := p.Parse("1 week ago")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !equalIgnoreSeconds(res, time.Now().Add(time.Hour*24*7*-1)) {
+		t.Error("Dates are not equal")
+	}
+}
+
+func TestParseWeeks(t *testing.T) {
+	p := CreateNaturalDateParser()
+	res, err := p.Parse("2 weeks ago")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !equalIgnoreSeconds(res, time.Now().Add(time.Hour*24*7*-2)) {
+		t.Error("Dates are not equal")
+	}
+}
+
+func TestParseMonth(t *testing.T) {
+	p := CreateNaturalDateParser()
+	res, err := p.Parse("2 months ago")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !equalIgnoreSeconds(res, time.Now().Add(time.Hour*24*30*-2)) {
+		t.Error("Dates are not equal")
+	}
+}
+
+func TestParseToday(t *testing.T) {
+	p := CreateNaturalDateParser()
+	res, err := p.Parse("today")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	now := time.Now()
+
+	if !(res.Day() == now.Day() &&
+		res.Month() == now.Month() &&
+		res.Year() == now.Year()) {
+		t.Error("Dates are not equal")
+	}
+}
